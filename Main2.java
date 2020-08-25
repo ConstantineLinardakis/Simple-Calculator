257392f64950b742597b2f505d80a1552a04ecc4
ConstantineLinardakis <54334383+ConstantineLinardakis@users.noreply.github.com>
2020-08-25 15:44:04 -0600

    Create Main2.java

---
+++ Main2.java
@@ -0,0 +1,22 @@
+public class Main2{
+    public int add(int x,int y){
+      int z;
+      z=x+y;
+      return z;
+    }
+    public int sub(int x,int y){
+      int z;
+      z=x-y;
+      return z;
+    }
+    public int div(int x,int y){
+      int z;
+      z=x/y;
+      return z;
+    }
+    public int multi(int x,int y){
+      int z;
+      z=x*y;
+      return z;
+    }
+}
